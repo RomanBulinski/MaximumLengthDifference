992d20628a7f61a6c04189bf290ecd9c90364ebb
Roman Bulinski <aboandrus@gmail.com>
2019-04-20 16:20:32 +0200

    Created structure of project

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+
+    public static int mxdiflg(String[] a1, String[] a2) {
+
+
+        // your code
+
+
+    }
+
+
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainTest {
+
+    @Test
+    public void test() {
+        System.out.println("mxdiflg Fixed Tests");
+        String[] s1 = new String[]{"hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"};
+        String[] s2 = new String[]{"cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"};
+        assertEquals(13, Main.mxdiflg(s1, s2)); // 13
+    }
+
+}
